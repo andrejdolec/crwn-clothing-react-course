13595ea67928a082a1107a914ae60f8743cad482
andrejdolec <dolevski.andrej@uklo.edu.mk>
2022-02-13 17:24:12 +0100

    finishing sign in handleSubmit method

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 import './sign-in.styles.scss';
 
@@ -16,10 +16,17 @@ class SignIn extends React.Component {
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
 
-        this.setState({ email: '', password: '' })
+        const { email, password } = this.state;
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({ email: '', password: '' })
+        } catch (error) {
+            console.log(error.message)
+        }
     }
 
     handleChange = event => {
@@ -43,11 +50,10 @@ class SignIn extends React.Component {
                         value={this.state.email} />
                     <FormInput
                         label="Password"
-                        name="email"
+                        name="password"
                         type="password"
                         handleChange={this.handleChange}
-                        value={this.state.email} />
-
+                        value={this.state.password} />
                     <div className="buttons">
                         <CustomButton type="submit"> Sign In </CustomButton>
                         <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign in with google</CustomButton>
