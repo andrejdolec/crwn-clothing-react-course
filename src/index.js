20fde650401a770590fb9427a19f791a566587e0
andrejdolec <dolevski.andrej@uklo.edu.mk>
2022-02-27 11:48:19 +0100

    added local storage with redux-persist

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import store from './redux/store';
+import storeAndPersistor from './redux/store';
+
 
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
-    <Provider store={store}>
+    <Provider store={storeAndPersistor.store}>
         <BrowserRouter>
-            <App />
+            <PersistGate persistor={storeAndPersistor.persistor}>
+                <App />
+            </PersistGate>
         </BrowserRouter>
     </Provider>
     , document.getElementById('root'));
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
 import logger from "redux-logger";
 
 import rootReducer from './root-reducer';
@@ -6,5 +7,8 @@ import rootReducer from './root-reducer';
 const middlewares = [logger];
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const persistor = persistStore(store);
 
-export default store;
+const storeAndPersistor = { store, persistor };
+
+export default storeAndPersistor;
